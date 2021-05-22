753f47cc601a25f13b2e8283085203724daf206a
young-nathan-a <81544458+young-nathan-a@users.noreply.github.com>
2021-05-22 01:06:34 -0500

    Update Natecoin.py

--- Natecoin.py
+++ Natecoin.py
@@ -46,21 +46,46 @@ class Blockchain():
         self.chain = chain
 
     def add(self,block):
-        self.chain.append(
-            {
-                'hash': block.hash(),
-                'previous': block.previous_hash,
-                'number': block.number,
-                'data': block.data,
-                'nonce': block.nonce
-                }
-            )
+        self.chain.append(block)
+
+    def remove(self, block):
+        self.chain.remove(block)
 
 
+    def mine(self, block):
+        try:
+            block.previous_hash = self.chain[-1].hash()
+        except IndexError: pass
+
+        while True:
+            if block.hash()[:self.difficulty] == "0" * self.difficulty:
+                self.add(block); break
+            else:
+                block.nonce += 1
+
+    def isValid(self):
+        for i in range(1, len(self.chain)):
+            _previous = self.chain[i].previous_hash
+            _current = self.chain[i-1].hash()
+            if _previous != _current or _current[:self.difficulty] != "0"*self.difficulty:
+                return False
+        return True
 
 def main():
-    block = Block("hello world",1)
-    print(block)
+    blockchain = Blockchain()
+    database = ["hello world", "what's up", "hello", "bye"]
+
+    num = 0
+    for data in database:
+        num += 1
+        blockchain.mine(Block(data,num))
+
+    for block in blockchain.chain:
+        print(block)
+
+    blockchain.chain[2].data = "NEW DATA"
+    blockchain.mine(blockchain.chain[2])
+    print(blockchain.isValid())
 
 if __name__ == '__main__':
     main()
